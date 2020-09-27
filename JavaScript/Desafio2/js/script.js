ff41f7848830d3453ff63c67925d22eebaf0e881
Renato Cesar Ais <C010256@manfrim.com.br>
2020-09-27 17:16:32 -0300

    js project

--- JavaScript/Desafio2/js/script.js
+++ JavaScript/Desafio2/js/script.js
@@ -1,15 +1,45 @@
 let allUsers = [];
+let strSearch = null;
+let stylebtn = null;
+let strField = null;
+
+strSearch = document.querySelector("#name");
+stylebtn = document.querySelector("#btn"); //style do botão
+let form = document.querySelector("form");
+
+strSearch.addEventListener("keyup", enablebtn);
+form.addEventListener("submit", prvSubmit);
 
 window.addEventListener("load", () => {
   fetchUser();
 });
 
+///Habilita e desabilita botão
+function enablebtn(event) {
+  let strtxt = event.target.value;
+  if (strtxt.length > 0) {
+    stylebtn.removeAttribute("disabled");
+  } else {
+    stylebtn.setAttribute("disabled", "disabled");
+  }
+  strField = event.target.value; ///variavel global
+}
+
+function prvSubmit(event) {
+  event.preventDefault();
+  fsearch();
+}
+
+function fsearch() {}
+
+///Cria Array com dados
 async function fetchUser() {
   const res = await fetch(
     "https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo"
   );
   const json = await res.json();
-  allUsers = json.map((results) => {
+
+  allUsers = json.results.map((results) => {
     return {
       name: results.name.first,
       lastname: results.name.last,
@@ -17,6 +47,4 @@ async function fetchUser() {
       age: results.dob.age,
     };
   });
-
-  console.log(allUsers);
 }
