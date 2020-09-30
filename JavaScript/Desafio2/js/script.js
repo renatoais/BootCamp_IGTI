e67f4600167e291e60d19fb2d0d04416dc6df270
Renato Cesar Ais <C010256@manfrim.com.br>
2020-09-29 22:46:48 -0300

    js project

--- JavaScript/Desafio2/js/script.js
+++ JavaScript/Desafio2/js/script.js
@@ -6,6 +6,9 @@ let strField = null;
 
 strSearch = document.querySelector("#name");
 stylebtn = document.querySelector("#btn"); //style do botão
+strUsersfound = document.querySelector("#userfound");
+strEstatfound = document.querySelector("#estatistica");
+
 let form = document.querySelector("form");
 
 strSearch.addEventListener("keyup", enablebtn);
@@ -18,18 +21,22 @@ window.addEventListener("load", () => {
 ///Habilita e desabilita botão
 function enablebtn(event) {
   let strtxt = event.target.value;
+
   if (strtxt.length > 0) {
     stylebtn.removeAttribute("disabled");
   } else {
     stylebtn.setAttribute("disabled", "disabled");
   }
   strField = event.target.value; ///variavel global
+
+  if (event.key === "Enter") prvSubmit(event);
 }
 
 function prvSubmit(event) {
   event.preventDefault();
   filter();
   renderUsers();
+  renderEstat();
 }
 
 ///Cria Array com dados
@@ -47,12 +54,6 @@ async function fetchUser() {
       age: results.dob.age,
     };
   });
-}
-
-function render() {
-  filter(strSearch);
-  renderUsers();
-  renderEstat();
 }
 
 function filter() {
@@ -96,29 +97,58 @@ function renderUsers() {
 
   userHTML += "</div>";
   tabUsers.innerHTML = userHTML;
+
+  strUsersfound;
 }
 
 function renderEstat() {
-  let userHTML = "<div>";
+  let statHTML = "<div>";
 
-  allUsers.forEach((results) => {
-    const { img, name, lastname, gender, age } = results;
+  if (allUsersFilter.length > 0) {
+    strUsersfound.textContent =
+      allUsersFilter.length + " Usuário(s) Encontrado(s)";
+    strEstatfound.textContent = "Estatísticas";
+  } else {
+    strUsersfound.textContent = "Nenhum Usuário Filtrado";
+    strEstatfound.textContent = "Nada a ser exibido";
+  }
 
-    const userHTMLstr = `
-    <div class='userf1'>
-      <div>
-        <img src="${img}" alt="${name}">
+  countedgenderF = allUsersFilter.filter(
+    (gender) => gender.gender.toUpperCase() === "FEMALE"
+  );
+
+  countedgenderM = allUsersFilter.filter(
+    (gender) => gender.gender.toUpperCase() === "MALE"
+  );
+
+  const sumAge = allUsersFilter.reduce((acc, cur) => {
+    return (acc += cur.age);
+  }, 0);
+
+  const avgAge = sumAge / (countedgenderF.length + countedgenderM.length);
+
+  const statHTMLstr = `
+  <div class='userg'>
+  <div>
+    <div>
+      <ul>
+        <li>Sexo Masculino : ${countedgenderM.length}</li>
+        <li>Sexo Feminino : ${countedgenderF.length} </li>
+        <li>Soma das idades : ${sumAge} </li>
+        <li>Média das idades : ${avgAge.toFixed(2)} </li>
+      </ul>
       </div>
-      <div>
-        <ul>
-          <li>Nome : ${name} ${lastname} ,${age} anos </li>
-        </ul>
       </div>
-    </div>  
-  `;
-    userHTML += userHTMLstr;
-  });
+  </div>  
+`;
+  statHTML += statHTMLstr;
 
-  userHTML += "</div>";
-  tabUsers.innerHTML = userHTML;
+  statHTML += "</div>";
+
+  if (allUsersFilter.length > 0) {
+    tabEstat.innerHTML = statHTML;
+  } else {
+    statHTML = "<div></div>";
+    tabEstat.innerHTML = statHTML;
+  }
 }
